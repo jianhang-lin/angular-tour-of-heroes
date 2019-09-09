026b640c711feb8a1b1ba9bfedb77792680b4eb8
JianhangLin <linjianhang1010@gmail.com>
2019-09-09 11:38:04 +0800

    feat(hero): get heroes with httpClient

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,20 +1,23 @@
 import { Injectable } from '@angular/core';
-import {HEROS} from './mock-heroes';
-import {Hero} from './heroes/hero';
-import {Observable, of} from 'rxjs';
-import {MessageService} from './message.service';
+import { HEROS } from './mock-heroes';
+import { Hero } from './heroes/hero';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-
+  private heroesUrl = 'api/heros';
   constructor(
+    private http: HttpClient,
     private messageService: MessageService
   ) { }
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes');
-    return of(HEROS);
+    // return of(HEROS);
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
   getHero(id: number): Observable<Hero> {
     this.messageService.add(`HeroService: fetched hero id = ${id}`);
