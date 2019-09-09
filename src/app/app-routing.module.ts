f515998848af6516797b89e8e6def5820a3c21cc
JianhangLin <linjianhang1010@gmail.com>
2019-09-09 10:23:46 +0800

    feat(app-routing): add a route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HeroesComponent} from './heroes/heroes.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
+import {HeroDetailComponent} from './hero-detail/hero-detail.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path: 'heroes',
     component: HeroesComponent
+  },
+  {
+    path: '/detail/:id',
+    component: HeroDetailComponent
   },
   {
     path: 'dashboard',
