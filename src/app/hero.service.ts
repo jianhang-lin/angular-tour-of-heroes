4dfbe90caa0e0e7e47239414dcaafd1108385fdf
JianhangLin <linjianhang1010@gmail.com>
2019-09-09 09:43:41 +0800

    feat(hero): inject the MessageService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,13 +2,16 @@ import { Injectable } from '@angular/core';
 import {HEROS} from './mock-heroes';
 import {Hero} from './heroes/hero';
 import {Observable, of} from 'rxjs';
+import {MessageService} from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(
+    private messageService: MessageService
+  ) { }
   getHeroes(): Observable<Hero[]> {
     return of(HEROS);
   }
